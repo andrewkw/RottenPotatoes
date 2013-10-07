45146d2ca35a0930bc47f97c6ddc1ce31ab2479a
saasbook <saasbook@saasbook.(none)>
2013-10-06 21:15:58 -0700

    Finished everything

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,42 +1,88 @@
 class MoviesController < ApplicationController
 
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
+    def show
+        id = params[:id] # retrieve movie ID from URI route
+        @movie = Movie.find(id) # look up movie by unique ID
+        # will render app/views/movies/show.<extension> by default
+    end
 
-  def index
-    @movies = Movie.order(params[:sort_param])
-    @sort = params[:sort_param]
-  end
+    def index
+        @all_ratings = Movie.all_ratings
+        already_visited = false
 
-  def new
-    # default: render 'new' template
-  end
+        if params[:sort_param]
+            @sort = params[:sort_param]
 
-  def create
-    @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
+        elsif session[:sort_param]
+            @sort = session[:sort_param]
+            already_visited = true
+        end
 
-  def edit
-    @movie = Movie.find params[:id]
-  end
+        if params[:ratings]
+            @ratings = params[:ratings]
 
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
+        elsif session[:ratings]
+            @ratings = session[:ratings]
+            already_visited = true
 
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
+        else
+            @all_ratings.each do |rating|
+                if @ratings
+                    @ratings[rating] = 0
+                else
+                    @ratings = {}
+                    @ratings[rating] = 1
+                end
+            end
+            already_visited = true
+        end
+	
+        if already_visited
+            redirect_to movies_path(:sort_param => @sort, :ratings => @ratings)
+        end
+
+        Movie.find(:all, :order => @sort ? @sort : :id).each do |movie|
+            if @ratings.keys.include? movie[:rating]
+                if @movies
+                    @movies << movie
+                else
+                    @movies = []
+                    @movies << movie
+                end
+            end
+        end
+
+        session[:sort_param] = @sort
+        session[:ratings] = @ratings
+
+    end
+
+    def new
+        # default: render 'new' template
+    end
+
+    def create
+        @movie = Movie.create!(params[:movie])
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
+
+    def edit
+        @movie = Movie.find params[:id]
+    end
+
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(params[:movie])
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+    end
 
 end
